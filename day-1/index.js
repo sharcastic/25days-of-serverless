786561843eae073acb13e2603d069101f5a6fd4a
Sharan Kumar <sharanblr@yahoo.co.in>
2019-12-06 15:19:34 +0530

    Day 1 solution

--- day-1/index.js
+++ day-1/index.js
@@ -1,15 +1,10 @@
-module.exports = async function (context, req) {
+const dreidel = [' נ (Nun)',' ג (Gimmel)',' ה (Hay)',' ש (Shin)'];
+
+module.exports = async function (context) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: dreidel[Math.floor(Math.random()*4)]
+    };
 };
