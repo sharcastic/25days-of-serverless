4d6e0e02f67a1acdde5df3ef684eb8b0bb03a2b6
Sharan Kumar <sharanblr@yahoo.co.in>
2019-12-06 15:28:46 +0530

    Revert "Day 1 solution"
    This reverts commit 786561843eae073acb13e2603d069101f5a6fd4a.

--- day-1/index.js
+++ day-1/index.js
@@ -1,10 +1,15 @@
-const dreidel = [' נ (Nun)',' ג (Gimmel)',' ה (Hay)',' ש (Shin)'];
-
-module.exports = async function (context) {
+module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: dreidel[Math.floor(Math.random()*4)]
-    };
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
 };
